deb726258b76c632be8badedc6621042da0b2110
Patrick Dearborn <Patrick Dearborn@10.132.244.123>
2019-11-21 16:02:09 -0700

    skeleton of extra stuff

--- src/view/TDView.java
+++ src/view/TDView.java
@@ -10,11 +10,17 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
@@ -25,38 +31,67 @@ public class TDView extends Application {
 	private GridPane grid;
 	private Canvas canvas;
 	private GraphicsContext gc;
+	private StackPane content;
 	private int rows;
 	private int columns;
-	private static final String FILE = "TDMap2";
+	private static final String FILE = "TDMap3";
+	
+	private static final int WIDTH = 50;
+	private static final int HEIGHT = 50;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		root = new BorderPane();
-		
-		StackPane content = new StackPane();
-		canvas = new Canvas();
-		gc = canvas.getGraphicsContext2D();
-		
-		createMap();
+		gc = createMap();
 		createLayout();
 		
-		content.getChildren().add(canvas);
-		content.getChildren().add(grid);
-		
-		root.setCenter(content);
-
 		scene = new Scene(root);
 		primaryStage.setTitle("Tower Defense");
 		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
 	
+	
 	public void createLayout() {
-		Button tower = new Button("Buy Tower");
-		GridPane.setConstraints(tower, 0,0);
-		root.setRight(tower);
+		VBox box = new VBox();
+		box.setSpacing(10);;
 		
+		Button tower1 = new Button("Tower 1");
+		GridPane.setConstraints(tower1, 0,0);
+		box.getChildren().add(tower1);
 		
+		Button tower2 = new Button("Tower 2");
+		GridPane.setConstraints(tower2, 0,1);
+		box.getChildren().add(tower2);
+		
+		Button tower3 = new Button("Tower 3");
+		GridPane.setConstraints(tower3, 0,2);
+		box.getChildren().add(tower3);
+		
+		Button tower4 = new Button("Tower 4");
+		GridPane.setConstraints(tower4, 0,3);
+		box.getChildren().add(tower4);
+		
+		Button tower5 = new Button("Tower 5");
+		GridPane.setConstraints(tower5, 0,4);
+		box.getChildren().add(tower5);
+		
+		root.setRight(box);
+		
+		HBox hbox = new HBox();
+		hbox.setSpacing(20);
+		
+		Label hp = new Label("HP: ");
+		TextField hpText = new TextField("100");
+		
+		Label gold = new Label("Gold: ");
+		TextField goldText = new TextField("100");
+		
+		hbox.getChildren().addAll(hp, hpText, gold, goldText);
+		
+		root.setTop(hbox);
+		
+		root.setBottom(new Button("New Wave >>>>"));
 	}
 	
 	public GraphicsContext createMap() { 
@@ -78,7 +113,7 @@ public class TDView extends Application {
                String line = in.nextLine();
                for (int i = 0; i < line.length(); i ++) {
             	   FileInputStream input = null;
-				   String road = "Ground.png";
+				   String road = "Road.png";
 				   String grass = "Grass.png";
 				   if (line.charAt(i) == '*') {
             		   input = new FileInputStream(grass);
