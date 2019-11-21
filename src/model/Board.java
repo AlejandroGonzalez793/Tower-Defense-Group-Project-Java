e34d134d8c103df86b808edaa96b9f7502d039c2
Ethan <Ethan_glasberg@hotmail.com>
2019-11-20 21:21:28 -0700

    Change board to be an array of Tiles instead of Entities

--- src/model/Board.java
+++ src/model/Board.java
@@ -2,7 +2,7 @@ package model;
 
 /**
  * The main object used for tracking the game state. Contains a 2D array
- * of <tt>Entity</tt> objects that each contain their own method of
+ * of <tt>Tile</tt> objects that each contain their own method of
  * drawing to the screen.
  *  
  * @author Ethan Glasberg (glasberg@email.arizona.edu)
@@ -10,60 +10,84 @@ package model;
  * @author Alex Gonzalez (aegonzalez793@email.arizona.edu)
  * @author Patrick Dearborn (pdearborn@email.arizona.edu) 
  * 
- * @see Entity
+ * @see Tile
  */
 public class Board {
-	private Entity board[][];
+	private Tile board[][];
 	
 	public Board(int width, int height) {
-		this.board = new Entity[height][width];
+		this.board = new Tile[height][width];
 	}
 	
 	/**
 	 * Directly get the underlying game array
 	 * @return the 2D entity array that is the state of the game
 	 */
-	public Entity[][] getBoard() {
+	public Tile[][] getBoard() {
 		return board;
 	}
 	
 	/**
-	 * Get the Entity at a specified coordinate
+	 * Get the Tile at a specified coordinate
 	 * 
-	 * @param x the x position to use
-	 * @param y the y position to use
-	 * @return the Entity at position x,y
+	 * @param row the row to get the Tile from
+	 * @param col the col to get the Tile from
+	 * @return the Tile at position (row,col) on the board
 	 */
-	public Entity getPosition(int x, int y) {
+	public Tile getTile(int row, int col) {
 		// TODO: Throw a specific exception other than OutOfBounds?
-		if (y < 0 || y > board.length) {
+		if (row < 0 || row > board.length) {
 			return null;
 		}
 		
-		if (x < 0 || x > board[0].length) {
+		if (col < 0 || col > board[0].length) {
 			return null;
 		}
 		
-		return board[y][x];
+		return board[row][col];
 	}
 	
 	/**
-	 * Set a position on the board to a specific Entity
+	 * Set a position on the board to a specific Tile
+	 * 
+	 * @param tile the Tile to set the position with
+	 * @param row the row to set the Tile at
+	 * @param col the col to set the Tile at
+	 */
+	public void setTile(Tile tile, int row, int col) {
+		// TODO: Throw a specific exception other than OutOfBounds?
+		if (row < 0 || row > board.length) {
+			return;
+		}
+		
+		if (col < 0 || col > board[0].length) {
+			return;
+		}
+		
+		board[row][col] = tile;
+	}
+	
+	/**
+	 * Get the entity at a specified coordinate
+	 * 
+	 * @param row the row to get the Entity from
+	 * @param col the col to get the Entity from
+	 * @return the Entity at position (row,col) on the board
+	 */
+	public Entity getEntity(int row, int col) {
+		Tile tile = getTile(row, col);
+		return tile.getEntity();
+	}
+	
+	/**
+	 * Set a position on the board to contain a specific Entity
 	 * 
 	 * @param entity the Entity to set the position with
-	 * @param x the x position to set the entity at
-	 * @param y the y position to set the entity at
+	 * @param row the row to set the Entity at
+	 * @param col the col to set the Entity at
 	 */
-	public void setPosition(Entity entity, int x, int y) {
-		// TODO: Throw a specific exception other than OutOfBounds?
-		if (y < 0 || y > board.length) {
-			return;
-		}
-		
-		if (x < 0 || x > board[0].length) {
-			return;
-		}
-		
-		board[y][x] = entity;
+	public void setEntity(Entity entity, int row, int col) {
+		Tile tile = getTile(row, col);
+		tile.setEntity(entity);
 	}
 }
