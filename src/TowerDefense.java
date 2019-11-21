1131c78b0836940977b736e11d50744680f2a6b6
jarodkylebristol <jarodkylebristol@email.arizona.edu>
2019-11-20 18:30:57 -0700

    basic window up

--- src/TowerDefense.java
+++ src/TowerDefense.java
@@ -1,9 +1,12 @@
+import javafx.application.Application;
+import view.TDView;
 
 public class TowerDefense {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// dev branch made 
+		// dev branch made
+		Application.launch(TDView.class, args);
 	}
 
 }
--- src/view/TDView.java
+++ src/view/TDView.java
@@ -1,5 +1,25 @@
 package view;
 
-public class TDView {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class TDView extends Application {
+	
+	private BorderPane root;
+	private Scene scene;
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		root = new BorderPane();
+		
+		scene = new Scene(root, 650, 450);
+		primaryStage.setTitle("Tower Defense");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
 }
