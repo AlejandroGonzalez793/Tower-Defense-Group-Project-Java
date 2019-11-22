f3e885d6700b5d97a2f51b590a7145b130c8dcef
Ethan <Ethan_glasberg@hotmail.com>
2019-11-21 20:39:12 -0700

    Add basic Game Controller

--- src/model/Projectile.java
+++ src/model/Projectile.java
@@ -2,16 +2,19 @@ package model;
 
 public class Projectile extends Entity {
 	public static final double DEFAULT_SPEED = 1.0;
+	public static final int DEFAULT_POWER = 1;
 	
 	private double speed;
+	private int power;
 	
-	public Projectile(int x, int y, int radius, double speed) {
+	public Projectile(int x, int y, int radius, double speed, int power) {
 		super(x, y, radius * 2, radius * 2);
 		this.speed = speed;
+		this.power = power;
 	}
 	
 	public Projectile(int x, int y, int radius) {
-		this(x, y, radius, DEFAULT_SPEED);
+		this(x, y, radius, DEFAULT_SPEED, DEFAULT_POWER);
 	}
 
 	public double getSpeed() {
@@ -20,5 +23,13 @@ public class Projectile extends Entity {
 	
 	public void setSpeed(double speed) {
 		this.speed = speed;
+	}
+	
+	public int getPower() { 
+		return power;
+	}
+	
+	public void setPower(int power) {
+		this.power = power;
 	}
 }
