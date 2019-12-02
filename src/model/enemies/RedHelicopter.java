a3781047aeb3898354854e8de8c96600b611f263
Ethan <Ethan_glasberg@hotmail.com>
2019-12-02 14:46:23 -0700

    Fix enemy speeds

--- src/model/enemies/RedHelicopter.java
+++ src/model/enemies/RedHelicopter.java
@@ -8,21 +8,20 @@ import javafx.scene.image.Image;
 public class RedHelicopter extends Enemy {
 	private static final int SPEED = 4;
 
-	public RedHelicopter(int x, int y, int width, int height, int speed, int health) {
-		super(x, y, width, height, speed, health);
+	public RedHelicopter(int x, int y, int width, int height, int health, int speed) {
+		super(x, y, width, height, health, speed);
 		try {
 			image = new Image(new FileInputStream("resources/images/enemies/red_helicopter.gif"));
-			//this.setImage(image);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public RedHelicopter(int x, int y, int width, int height, int speed) {
-		this(x, y, width, height, speed, DEFAULT_HEALTH);
+		this(x, y, width, height, DEFAULT_HEALTH, speed);
 	}
 	
 	public RedHelicopter(int x, int y) {
-		this(x, y, 50, 50, SPEED, DEFAULT_HEALTH);
+		this(x, y, 50, 50, DEFAULT_HEALTH, SPEED);
 	}
 }
