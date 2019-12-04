371c96d659fb6ded5fc10dd1ab074f2aeae31445
jarodkylebristol <jarodkylebristol@email.arizona.edu>
2019-12-04 16:03:33 -0700

    added pre-set values to radius and speed to towers and projectils

--- src/model/projectiles/Projectile.java
+++ src/model/projectiles/Projectile.java
@@ -68,7 +68,7 @@ public class Projectile extends Entity {
 
 	public void update(int dxx, int dyy) {
 		x = dx;
-		y = dy; // issues
+		y = dy; // issues here 
 		System.out.println(dxx);
 		System.out.println(dyy);
 	}
