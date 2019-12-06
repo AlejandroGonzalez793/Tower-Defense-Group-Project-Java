e91d143e7e1577e62214ba5c7dcae06d8797f9d9
Ethan <Ethan_glasberg@hotmail.com>
2019-12-05 22:19:16 -0700

    Only iterate through enemies if tower can generate projectile

--- src/model/GameState.java
+++ src/model/GameState.java
@@ -69,12 +69,12 @@ public class GameState extends Observable {
 		}
 
 		for (Tower tower : towers) {
-			for (Enemy enemy : enemies) {
-				double x = Math.abs(enemy.getX() - tower.getX());
-				double y = Math.abs(enemy.getY() - tower.getY());
-				double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-				if (distance <= tower.getRadius()) {
-					if (tower.generateProjectile(ticks)) {
+			if (tower.generateProjectile(ticks)) {
+				for (Enemy enemy : enemies) {
+					double x = Math.abs(enemy.getX() - tower.getX());
+					double y = Math.abs(enemy.getY() - tower.getY());
+					double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+					if (distance <= tower.getRadius()) {
 						Projectile projectile = tower.getProjectile();
 
 						if (x < y) {
