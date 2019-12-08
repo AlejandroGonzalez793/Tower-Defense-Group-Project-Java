de76b1c13aa4e81b51b7d2ea89d7c626d3ddb09b
Ethan <Ethan_glasberg@hotmail.com>
2019-12-07 20:40:12 -0700

    Make getting waves static

--- src/model/Waves.java
+++ src/model/Waves.java
@@ -1,6 +1,8 @@
 package model;
 
-import javafx.scene.shape.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+
 import model.enemies.Balloon;
 import model.enemies.Drifblim;
 import model.enemies.Enemy;
@@ -10,57 +12,34 @@ import model.enemies.Pterosaur;
 import model.enemies.RedHelicopter;
 
 public class Waves {
-	private GameState gameState;
 	
-	public Waves(GameState gameState) {
-		this.gameState = gameState;
-	}
-	
-	public void getWave(int waveNum) {
-		Rectangle start = gameState.getStart().getRectangle();
-		int x = (int)start.getX();
-		int y = (int)start.getY();
+	public static List<Enemy> getWave(int waveNum, int x, int y) {
+		List<Enemy> wave = new ArrayList<>();
 		
 		if (waveNum == 0) {
-			Enemy enemy = new Balloon(x, y);
-			Enemy enemy2 = new HotAirBalloon(x, y);
-			Enemy enemy3 = new GreenPlane(x, y);
-			gameState.addEnemy(enemy);
-			gameState.addEnemy(enemy2);
-			gameState.addEnemy(enemy3);
+			wave.add(new Balloon(x, y));
+			wave.add(new HotAirBalloon(x, y));
+			wave.add(new GreenPlane(x, y));
 		} else if (waveNum == 1) {
-			Enemy enemy = new Balloon(x, y);
-			Enemy enemy2 = new HotAirBalloon(x, y);
-			Enemy enemy3 = new RedHelicopter(x, y);
-			gameState.addEnemy(enemy);
-			gameState.addEnemy(enemy2);
-		    gameState.addEnemy(enemy3);
+			wave.add(new Balloon(x, y));
+			wave.add(new HotAirBalloon(x, y));
+			wave.add(new RedHelicopter(x, y));
 		} else if (waveNum == 2) {
-			Enemy enemy = new GreenPlane(x, y);
-			Enemy enemy2 = new Pterosaur(x, y);
-			gameState.addEnemy(enemy2);
-			gameState.addEnemy(enemy);
+			wave.add(new GreenPlane(x, y));
+			wave.add(new Pterosaur(x, y));
 		} else if (waveNum == 3) {
-			Enemy enemy = new RedHelicopter(x, y);
-			Enemy enemy2 = new Drifblim(x, y);
-			Enemy enemy3 = new GreenPlane(x, y);
-			gameState.addEnemy(enemy);
-			gameState.addEnemy(enemy2);
-			gameState.addEnemy(enemy3);
+			wave.add(new RedHelicopter(x, y));
+			wave.add(new Drifblim(x, y));
+			wave.add(new GreenPlane(x, y));
 		} else if (waveNum == 4) {
-			Enemy enemy = new Pterosaur(x, y);
-		    Enemy enemy2 = new GreenPlane(x, y);
-		    Enemy enemy3 = new RedHelicopter(x, y);
-		    Enemy enemy4 = new Balloon(x, y);
-		    Enemy enemy5 = new HotAirBalloon(x, y);
-		    Enemy enemy6 = new Drifblim(x, y);
-		    gameState.addEnemy(enemy);
-		    gameState.addEnemy(enemy2);
-		    gameState.addEnemy(enemy3);
-		    gameState.addEnemy(enemy4);
-		    gameState.addEnemy(enemy5);
-		    gameState.addEnemy(enemy6);
+			wave.add(new Pterosaur(x, y));
+			wave.add(new GreenPlane(x, y));
+			wave.add(new RedHelicopter(x, y));
+			wave.add(new Balloon(x, y));
+			wave.add(new HotAirBalloon(x, y));
+			wave.add(new Drifblim(x, y));
 		}
+		
+		return wave;
 	}
-
 }
