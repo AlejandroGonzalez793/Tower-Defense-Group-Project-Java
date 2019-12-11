6103bc34120f79280eff3ac2c2668a0445a8ddcb
Ethan <Ethan_glasberg@hotmail.com>
2019-12-11 11:45:19 -0700

    Add extended JavaDoc comment to TowerDefense.java

--- src/TowerDefense.java
+++ src/TowerDefense.java
@@ -3,9 +3,47 @@ import view.TDView;
 
 /**
  * The TowerDefense class will launch the Tower Defense game "Flying Defense!"
- * The game comes with three stages and a map selection that allows users
- * to play on custom maps. Various towers and enemies are present along with 
- * an orchestrated tone that will play in the background.
+ * The game comes with three stages and a map selection that allows users to
+ * play on custom maps. Various towers and enemies are present along with an
+ * orchestrated tone that will play in the background.
+ * 
+ * <p>
+ * <b>Compiation: </b>javac *.java model/*.java model/enemies/*.java
+ * model/projectiles/*.java model/towers/*.java util/*.java view/*.java
+ * </p>
+ * 
+ * <p>
+ * <b>Execution: </b>java TowerDefense
+ * </p>
+ * 
+ * <p>
+ * The game also allows the users to provides a custom map format for users to
+ * create their own maps. A custom map must be a text file that starts with the
+ * number of rows on one line, number of columns on the next line and then an
+ * ASCII representation of the board using the following characters:<br>
+ * <br>
+ * `*` - free space (a place that a tower can be placed)<br>
+ * `-` - path space (where the enemies can travel)<br>
+ * `+` - start of the path (where the enemies will start)<br>
+ * `=` - end of the path (where the enemies will stop) <br>
+ * <br>
+ * <b>Example:</b><br>
+ * 16<br>
+ * 13<br>
+ * ****----**=*****<br>
+ * ****-**-**-*****<br>
+ * **---**-**-----*<br>
+ * **-****-******-*<br>
+ * **-****-******-*<br>
+ * **---**-**-----*<br>
+ * ****-**-**-*****<br>
+ * ****-**-**-*****<br>
+ * **---**-**-*----<br>
+ * **-****-**-*-**-<br>
+ * +--****-**---**-<br>
+ * *******-*******-<br>
+ * *******---------<br>
+ * </p>
  * 
  * @author Ethan Glasberg (glasberg@email.arizona.edu)
  * @author Jarod Bristol (jarodkylebristol@email.arizona.edu)
